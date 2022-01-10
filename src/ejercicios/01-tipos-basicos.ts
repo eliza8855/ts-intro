b408f80b947c0b153346574d212cf0383f0e2b5e
eliza8855 <elopezvalerin@gmail.com>
2022-01-10 12:57:14 -0500

    segunda actualizacion

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,13 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Elizabeth';
+let hp: number | string  = 95;
+let estaVivo: boolean = true;
+
+hp = 'FULL';
+
+console.log (nombre, hp);
+
